44d52dbc1e645badd841cc6075e969331ce4bf7f
Ståle Semb Hauknes <stale.hauknes@evry.com>
2018-09-05 18:23:42 +0200

    Changed config sample.

--- config-sample.js
+++ config-sample.js
@@ -1,10 +1,13 @@
 module.exports = {
-  area: 'FI', // see http://www.nordpoolspot.com/maps/
-  currency: 'EUR', // can also be 'DKK', 'NOK', 'SEK'
-  highTreshold: 60, // send event when price > highTreshold EUR/MWh
-  lowTreshold: 30, // send event when price < lowTreshold EUR/MWh
-  maxHighHours: 24, // max consecutive high hours
+  area: 'Tr.heim', // see http://www.nordpoolspot.com/maps/
+  currency: 'NOK', // can also be 'DKK', 'NOK', 'SEK'
+  maxHighHours: 4, // max consecutive high hours
   maxLowHours: 24, // max consecutive low hours
-  vatPercent: 24, // if you want prices including value-added tax (VAT), enter the percentage here
-  iftttKey: 'CHANGE' // see https://ifttt.com/services/maker_webhooks/settings
+  veryLowOffset: 1.5, // Temperature offset when the price is very low
+  lowOffset: 0.5, // Temperature offset when the price is low
+  highOffset: -0.5,  // Temperature offset when the price is high
+  veryHighOffset: -1.5, // Temperature offset when the price is very high
+  hassPassword: 'PASSWORD', // API password to Home Assistant
+  hassUrl: 'http://www.example.com/', // URL to Home Assistant. Remember the slash
+  hassSensorName: 'sensor.power_price' // Name of the sensor
 };
